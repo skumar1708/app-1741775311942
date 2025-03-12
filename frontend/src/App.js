8290d027e56de0ae1645abaf8b86ec9b808aa85a
Sharvan Sharma <itbhu.sk@gmail.com>
2025-03-12 16:01:26 +0530

    Automated commit: frontend/src/App.js

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import GamePage from './pages/GamePage';
+import './App.css';
+
+function App() {
+  return (
+    <div className="app-container">
+      <GamePage />
+    </div>
+  );
+}
+
+export default App;
