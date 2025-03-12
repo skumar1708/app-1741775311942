e8a6b02ae756e19f36a617f99aa8a23e6a16b379
Sharvan Sharma <itbhu.sk@gmail.com>
2025-03-12 16:01:39 +0530

    Automated commit: frontend/src/components/FlowerEffect.js

---
+++ frontend/src/components/FlowerEffect.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function FlowerEffect() {
+  return (
+    <div className="flower-effect">
+      <div className="flower"></div>
+    </div>
+  );
+}
+
+export default FlowerEffect;
